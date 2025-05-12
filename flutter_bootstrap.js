de95b58d681ba51317a358744bf3be67ac7d80cd
Barney-Jackson <109702684+Barney-Jackson@users.noreply.github.com>
2025-05-12 14:44:37 +1000

    Take 3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2866719436"
+    serviceWorkerVersion: "3401820482"
   }
 });
