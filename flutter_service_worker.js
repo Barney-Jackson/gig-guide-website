edf23f16a21263bfd07829f5400f49f9de2af8ad
Barney-Jackson <109702684+Barney-Jackson@users.noreply.github.com>
2025-06-29 16:37:17 +1000

    cors user change

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,7 +10,7 @@ const RESOURCES = {"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "index.html": "1c0e044b1ebdecfe5369e4a3cc17bb19",
 "/": "1c0e044b1ebdecfe5369e4a3cc17bb19",
 "manifest.json": "42ad17f582b68e1e28a2fa5f0b4f33be",
-"flutter_bootstrap.js": "9d8a006a6bc2eee86b4d60a0cbcf88c2",
+"flutter_bootstrap.js": "4cf7f099b36cecae33f0d9ade209f234",
 "canvaskit/skwasm.wasm": "f0dfd99007f989368db17c9abeed5a49",
 "canvaskit/canvaskit.js": "6cfe36b4647fbfa15683e09e7dd366bc",
 "canvaskit/canvaskit.js.symbols": "68eb703b9a609baef8ee0e413b442f33",
@@ -35,7 +35,7 @@ const RESOURCES = {"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "6be1e5f97a5ce1258ff6790316168ea3",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"main.dart.js": "5ef24e8c36ba4ae980c5a28fb78b4488"};
+"main.dart.js": "fef81ad2f742878dd5ad9d31a5b0c436"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
